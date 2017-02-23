daa96782304a241c639fe61e4f29e0e35503587c
wangweiwei <will_workhard@163.com>
2017-02-24 00:27:53 +0800

    nodejs 启动服务

---
+++ Nodejs/start/server.js
@@ -0,0 +1,14 @@
+const http = require("http")
+
+const hostname = "127.0.0.1"
+const port = "9999"
+
+var server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-type", 'text/plain');
+    res.end('hello world\n');
+})
+
+server.listen(port, hostname, () => {
+    console.log(`server is running http://${hostname}:${port}/`)
+} )
