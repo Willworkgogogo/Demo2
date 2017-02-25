25d29cbda7ee85f912c6aa41dd4d0756756c963e
wangweiwei <will_workhard@163.com>
2017-02-25 23:12:11 +0800

    待解决窗口resize时如何调整

--- UEDdemo/效果实现/遮罩弹框/zhezhao.js
+++ UEDdemo/效果实现/遮罩弹框/zhezhao.js
@@ -1,18 +1,42 @@
 window.onload = function() {
     var loginBtn = document.querySelector(".login-btn")
+    
 
     loginBtn.onclick = function() {
-        createAlert()
+        createAlert();
+
+        var zhezhao = document.querySelector(".zhezhao"),
+            closeBtn = document.querySelector(".close-btn"),
+            alertBox = document.querySelector(".alert-box")
+        
+        //关闭
+        zhezhao.onclick = closeBtn.onclick = removeZhezhao;
+        
+        //esc 关闭
+        document.onkeydown = function(ev) {
+            var ev = ev || window.event;
+            if(document.querySelector(".zhezhao")){
+                if (ev.keyCode == 27) {
+                    removeZhezhao();
+                }
+            }
+        }
+
+        //removeZhezhao 移除节点
+        function removeZhezhao() {
+            var body = document.querySelector("body");
+            body.removeChild(zhezhao);
+            body.removeChild(alertBox);
+        }
     };
 
     function createAlert() {
         //获取页面的宽高
-        
         var documentElement = document.body ? document.body : document.documentElement
             scrollHeight = documentElement.scrollHeight,
             scrollWidth = documentElement.scrollWidth,
-            clientHeight = documentElement.clientHeight,
-            clientWidth  = documentElement.clientWidth
+            clientHeight = document.documentElement.clientHeight,
+            clientWidth  = document.documentElement.clientWidth
  
         //bg层
         var zhezhao = document.createElement("div");
@@ -22,10 +46,17 @@ window.onload = function() {
         document.body.appendChild(zhezhao);
 
         //居中弹框
-        var alertBox = document.createElement("div");
+        var alertBox = document.createElement("div"),
+            closeBtn = document.createElement("span")
+
         alertBox.className = "alert-box";
+        closeBtn.className = "close-btn";
+        closeBtn.innerHTML = "close";
+        alertBox.appendChild(closeBtn);
+
         //绝对定位的left，top值； (总宽-div宽)/2
-        alertBox.style.left = (clientWidth - 500)/2;
-        alertBox.style.top = (clientHeight - 300)/2;
+        alertBox.style.left = (clientWidth - 500)/2 + "px";
+        alertBox.style.top = (clientHeight - 300)/2 + "px";
+        document.body.appendChild(alertBox);
     }
 }
