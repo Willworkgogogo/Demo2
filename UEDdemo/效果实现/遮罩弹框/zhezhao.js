49fd0dac15f1e2f887bd35408929d73340d248a4
wangweiwei <will_workhard@163.com>
2017-02-25 22:21:56 +0800

    修改

--- UEDdemo/效果实现/遮罩弹框/zhezhao.js
+++ UEDdemo/效果实现/遮罩弹框/zhezhao.js
@@ -17,6 +17,15 @@ window.onload = function() {
         //bg层
         var zhezhao = document.createElement("div");
         zhezhao.className = "zhezhao";
-        
+        zhezhao.style.width = scrollWidth + "px";
+        zhezhao.style.height = scrollHeight + "px";
+        document.body.appendChild(zhezhao);
+
+        //居中弹框
+        var alertBox = document.createElement("div");
+        alertBox.className = "alert-box";
+        //绝对定位的left，top值； (总宽-div宽)/2
+        alertBox.style.left = (clientWidth - 500)/2;
+        alertBox.style.top = (clientHeight - 300)/2;
     }
 }
