05a7a78d35bb3f85da9a76cea0afa3d765985145
wangweiwei <will_workhard@163.com>
2017-03-22 21:19:46 +0800

    验证处理

--- Angular-0/hello-angular/src/app/login/login.component.ts
+++ Angular-0/hello-angular/src/app/login/login.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+// import { AuthService } from '../core/auth.service';
 
 @Component({
   selector: 'app-login',
-  template: `
-    <p>
-      login Works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  // 在providers中配置AuthService
+  // providers: [AuthService]
 })
 export class LoginComponent implements OnInit {
+  username = "wangweiwei";
+  password = "123abc";
 
-  constructor() { }
+  // 在构造函数中将AuthService示例注入到成员变量service中， 并将其私有化
+  constructor(@Inject('auth') private service) {
+  }
 
   ngOnInit() {
   }
 
+  // onClick() {
+  //   console.log('auth result is: ' + this.service.loginWithCredentials(this.username, this.password))
+  // }
+
+  onSubmit(formValue) {
+    console.log('auth result is: ' + this.service.loginWithCredentials(formValue.login.username, formValue.login.password) )
+  }
 }
