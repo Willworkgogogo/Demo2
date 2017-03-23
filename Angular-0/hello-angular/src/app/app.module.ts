ffcef5e697336224b6cdf9b826a9252710c335ed
wangweiwei <will_workhard@163.com>
2017-03-23 15:34:09 +0800

    todolist

--- Angular-0/hello-angular/src/app/app.module.ts
+++ Angular-0/hello-angular/src/app/app.module.ts
@@ -2,34 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { LoginComponent } from './login/login.component';
 import { AuthService } from './core/auth.service';
+import { routing } from  './app.routes';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    LoginComponent
+    LoginComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot([
-      {
-        path:'',
-        redirectTo: 'login',
-        pathMatch: 'full'
-      },
-      {
-        path: 'login',
-        component: LoginComponent
-      }
-    ])
+    routing
   ],
   providers: [
     {provide: 'auth', useClass: AuthService }
